06805531e4e1487e8085c3f88acf48766e7d0da7
stefano <stefano_vannucci@hotmail.com>
2023-10-03 16:07:36 +0200

    actualizacion del app

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['https://bayut-production.s3.eu-central-1.amazonaws.com']
+  }
 }
 
 module.exports = nextConfig
